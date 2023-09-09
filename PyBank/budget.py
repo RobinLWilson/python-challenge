c652cbd32f02c74903dc45d5b4070f2f17734571
Robin Wilson <139357402+RobinLWilson@users.noreply.github.com>
2023-09-08 22:04:21 -0400

    9/8/23 update

--- PyBank/budget.py
+++ PyBank/budget.py
@@ -8,6 +8,8 @@ month= []
 profit_loss = []
 average_profit_loss = []
 change_profit_loss = []
+shift_profit_loss = []
+min_max_dict = {}
 
 # Read the csv file & split data on ,:
 with open(budget_csv) as csvfile:
@@ -27,7 +29,7 @@ with open(budget_csv) as csvfile:
         profit_loss.append(int(row[1]))
         change_profit_loss.append(int(row[1]) - previous_value)
         previous_value = int(row[1])
-        
+      
 #print(change_profit_loss)
 def sum(numbers):
     total = 0
@@ -42,13 +44,38 @@ def average(numbers):
         total += number
     return total / length 
 
-max_profit_loss = max(profit_loss)
-min_profit_loss = min(profit_loss)
+average_change = round(average(change_profit_loss), 2)
 
-#average = sum(total_profit_loss) / len(total_profit_loss)
+shift_profit_loss = change_profit_loss
+shift_profit_loss.insert(0, "00000")
+
+for date in month:
+    for value in shift_profit_loss:
+        min_max_dict[date] = value
+        shift_profit_loss.remove(value)
+        break
+
+greatest_increase = max(min_max_dict.items(), key=lambda x: int(x[1]))
+greatest_decrease = min(min_max_dict.items(), key=lambda x: int(x[1]))
+
 print(f"Total Months: {len(month)}")
 print(f"Total: ${sum(profit_loss)}")   
-print(f"Average Change: ${average(change_profit_loss)}")
+print(f"Average Change: ${average_change}")
+print("Greatest Increase in Profits: " + str(greatest_increase))
+print("Greatest Decrease in Profits: " + str(greatest_decrease))
 
-print("The largest is: ", max_profit_loss)
-print("The smallest is: ", min_profit_loss)
+# Set variable for output file
+output_file = os.path.join('C:/Users/ormon/OneDrive/Desktop/python-challenge/PyBank/analysis/', 'PyBank_Results.txt')
+
+#  Open the output file
+with open(output_file, "w") as textfile:
+    textfile.write(f"Total Months: {len(month)}")
+    textfile.write("\n")
+    textfile.write(f"Total: ${sum(profit_loss)}")  
+    textfile.write("\n")
+    textfile.write(f"Average Change: ${average_change}")
+    textfile.write("\n")
+    textfile.write("Greatest Increase in Profits: " + str(greatest_increase))
+    textfile.write("\n")
+    textfile.write("Greatest Decrease in Profits: " + str(greatest_decrease))
+   
